665aed1e19a60ac42b2289beea21dd6da57d4742
Andrew Lovett-Barron <andrew@relaystudio.com>
2017-12-16 14:42:26 -0700

    Directly writing headers at this point...

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(passport.session());
 app.use(cors({credentials: true, origin: ['http://127.0.0.1:4000','https://mulligan-fund.github.io/']}));
 app.use(methodOverride());
 
-
+var heroku = process.env.HEROKU_TRUE || false
 
 // Mongoose
 var schema = require('./schema.js');
@@ -114,10 +114,12 @@ function ensureAuthenticated(req, res, next) {
 // }
 
 app.options('*', cors({credentials: true, origin: ['http://127.0.0.1:4000','https://mulligan-fund.github.io']})); // Setup CORS option
-app.options('*', function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader("Access-Control-Allow-Headers", "*");
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', heroku ? 'https://mulligan-fund.github.io' : 'http://127.0.0.1:4000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'appid, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
 });
 
 
